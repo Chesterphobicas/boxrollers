5ca25c92097cfcf6b01471b0a80b780fd8c38503
Kit Withers <kitwithers@Catherines-Air.gateway>
2017-01-03 19:49:56 +1030

    started the game.

---
+++ Assets/scripts/EmemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmemyController : MonoBehaviour
+{
+	private	Transform player;
+	public int moveSpeed = 3;
+	public int rotationSpeed = 3;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player").transform;
+	}
+
+
+	void Update ()
+	{
+		//rotate to look at the player
+		transform.rotation = Quaternion.Slerp (transform.rotation,
+			Quaternion.LookRotation (player.position - transform.position), rotationSpeed * Time.deltaTime);
+
+		//move towards the player
+		transform.position += transform.forward * moveSpeed * Time.deltaTime;
+	}
+}
---
+++ Assets/scripts/JeraldMover.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JeraldMover : MonoBehaviour
+{
+	public float jumpAmount = 5.0f;
+	public float speed;
+	private Rigidbody rb;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		float jumpValue = 0.0f;
+		if (Input.GetKey (KeyCode.Space)) {
+			if (transform.position.y < 1) {
+				jumpValue = jumpAmount;
+			}
+		}
+
+		Vector3 movment = new Vector3 (moveHorizontal, jumpValue, moveVertical);
+		rb.AddForce (movment * speed);
+	}
+
+}
